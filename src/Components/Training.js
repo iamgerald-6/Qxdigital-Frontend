62d7f937363f9331bfe47683c78b7a3a16188dd8
Gerald Adjei <geraldsix89@gmail.com>
2023-10-25 22:11:00 +0100

    responsive

--- src/Components/Training.js
+++ src/Components/Training.js
@@ -32,9 +32,9 @@ export const Training = () => {
                             <div class="bg-orange-500 h-3 rounded-full" style={{"width": "45%"}}></div>
                         </div>
                      </div>
-                     
-
                 </div>
+
+
                 <div className=' bg-orange-50 border border-stone-800 h-[32vh] lg:w-[18vw] lg:ms-36 mt-7 shadow-md shadow-orange-200 relative rounded-xl'>
                     <h1 className='text-center text-xl font-family uppercase mt-5'>Applied</h1>
                     <div className='flex justify-center   h-[14vh] items-end '>
@@ -47,6 +47,7 @@ export const Training = () => {
                     </div>
                     <h3 className='text-lg lg:text-start text-center font-family lg:ms-9 mt-1'>Acceptance Rate</h3>
                 </div>
+
 
                 <div className='border bg-orange-50 border-stone-800 h-[32vh] lg:ms-20 lg:w-[18vw] relative mt-7 shadow-md shadow-orange-200 rounded-xl'>
                     <h1 className='text-center text-xl uppercase font-family mt-5'>pending</h1>
@@ -76,18 +77,18 @@ export const Training = () => {
             </div>
 
 
-            <div className='grid lg:grid-cols-3'>
-                <div className='border mt-5 h-[44vh] flex justify-center flex-col items-center rounded-tr-[48px] lg:col-span-1 shadow-md shadow-slate-800'>
+            <div className='lg:flex lg:flex-row flex-col'>
+                <div className='border mt-5 h-[44vh] flex justify-center flex-col items-center rounded-tr-[48px]  lg:w-[68vw] shadow-md shadow-slate-800'>
                     <button className='w-[40vw] lg:w-[27vw] shadow-md shadow-orange-300 py-2 border uppercase hover:bg-orange-50 transition ease-out duration-300 flex font-family justify-evenly'>Cv Review Status <SiGoogledocs className=' mt-1' /></button>
-                    <button  className='w-[40vw] lg:w-[27vw] text-center shadow-md shadow-orange-300 mt-4 py-2 border uppercase hover:bg-orange-50 transition ease-out font-family duration-300 flex justify-evenly'>Messages <BsFillChatDotsFill className='ms-12 mt-1'/></button>
+                    <button  className='w-[40vw] lg:w-[27vw] text-center shadow-md shadow-orange-300 mt-4 py-2 border uppercase hover:bg-orange-50 transition ease-out font-family duration-300 flex justify-evenly'>Messages <BsFillChatDotsFill className='lg:ms-12  mt-1'/></button>
                     <NavLink to='/trainingoffer' style={buttonStyle} className='w-[40vw] lg:w-[27vw] shadow-md shadow-orange-300  mt-4 py-2 border uppercase flex font-family justify-evenly'>Training Offers<HiBriefcase className='  mt-1' /></NavLink>
                 </div>
-                <div className='lg:col-span-2'>
+                <div className=''>
 
                     <hr className='lg:ms-9 ms-3 w-[92vw] shadow-md mt-3 border-2 lg:w-[63vw]'></hr>
                     <h4 className='ms-9 lg:text-start text-center mt-1 text-xl font-bold font-family '>Recommended for you</h4>
 
-                    <div className='flex flex-col h-60 overflow-y-scroll'>
+                    <div className='h-60 overflow-y-scroll'>
                         <div className='flex justify-between'>
                             {/* <div className=' lg:w-[15vw] py-2 border ms-10 mt-5'>
                                 <h1 className='text-center text-md flex justify-between  '><div className='bg-black rounded-full'></div>UI/UX Designer <RiArrowDropDownFill onClick={handleChange} className={`cursor-pointer mt-1 ${open ? 'rotate-180' :''}`}/></h1>
